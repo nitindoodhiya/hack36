0122fc9c192db0f9b609d27e984ef2b8563c734f
Nick Nitin <nitindoodhiya@gmail.com>
2019-07-11 20:52:06 +0530

    Add files via upload

---
+++ software.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="en">
+  
+  <head>
+      
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title >Software installation</title>
+  </head>
+  <style>
+img{
+     vertical-align: middle;
+    width: 200px;
+    height: 200px;
+     margin-left: 20px;
+    margin-right: 20px;
+}
+ 
+   body{
+       background:cornsilk;
+   }
+  
+    </style>
+
+    <body>
+       
+
+
+    <div class="cls">
+        <center>
+    <h1 style="color mediumblue" style="font: Verdana" >Software Portal</h1>
+        </center>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+    <section>
+    <div class="container">
+  <div class="row">
+
+    <div class="col-sm">
+    <a href="software_image/geary.jpg">
+     <img src="software_image/geary.jpg"  alt="Geary" width="300" height="200">
+    </a>
+    <h9 class="font-weight-bold" class="font-italic" style="font-family:Verdana;">Geary</h9>
+  <a href="zips/geary.zip" class="btn btn-outline get-repo-btn" data-ga-click="Repository,download zip,location:repo overview">
+  Download zip
+  </a>
+    </div>
+     <div class="col-sm">
+    <a href="software_image/audacity.jpg">
+     <img src="software_image/audacity.jpg"  alt="audacity" width="300" height="200">
+    </a>
+    <h9 class="font-weight-bold" class="font-italic" style="font-family:Verdana;">Audacity</h9>
+  <a href="zips/audacity.zip" class="btn btn-outline get-repo-btn" data-ga-click="Repository,download zip,location:repo overview">
+  Download zip
+  </a>
+    </div>
+     <div class="col-sm">
+    <a href="software_image/chromium.jpg">
+     <img src="software_image/chromium.jpg"  alt="chromium" width="300" height="200">
+    </a>
+    <h9 class="font-weight-bold" class="font-italic" style="font-family:Verdana;">Chromium</h9>
+  <a href="zips/chromium.zip" class="btn btn-outline get-repo-btn" data-ga-click="Repository,download zip,location:repo overview">
+  Download zip
+  </a>
+    </div>
+     <div class="col-sm">
+    <a href="software_image/gimp.jpg">
+     <img src="software_image/gimp.jpg"  alt="gimp" width="300" height="200">
+    </a>
+    <h9 class="font-weight-bold" class="font-italic" style="font-family:Verdana;">Gimp</h9>
+  <a href="zips/gimp.zip" class="btn btn-outline get-repo-btn" data-ga-click="Repository,download zip,location:repo overview">
+  Download zip
+  </a>
+    </div>
+     <div class="col-sm">
+    <a href="software_image/git.jpg">
+     <img src="software_image/git.jpg"  alt="git" width="300" height="200">
+    </a>
+    <h9 class="font-weight-bold" class="font-italic" style="font-family:Verdana;">Git</h9>
+  <a href="zips/git.zip" class="btn btn-outline get-repo-btn" data-ga-click="Repository,download zip,location:repo overview">
+  Download zip
+  </a>
+
+    </div>
+        <br><br>
+    <br><br>
+
+     <div class="col-sm">
+    <a href="software_image/NetBeans.jpg">
+     <img src="software_image/NetBeans.jpg"  alt="NetBeans" width="300" height="200">
+    </a>
+    <h9 class="font-weight-bold" class="font-italic" style="font-family:Verdana;">NetBeans</h9>
+  <a href="zips/netbeans.zip" class="btn btn-outline get-repo-btn" data-ga-click="Repository,download zip,location:repo overview">
+  Download zip
+  </a>
+    </div>
+     <div class="col-sm">
+    <a href="software_image/nodejs.jpg">
+     <img src="software_image/nodejs.jpg"  alt="Nodejs" width="300" height="200">
+    </a>
+    <h9 class="font-weight-bold" class="font-italic" style="font-family:Verdana;">Nodejs</h9>
+  <a href="zips/node_js.zip" class="btn btn-outline get-repo-btn" data-ga-click="Repository,download zip,location:repo overview">
+  Download zip
+  </a>
+    </div>
+     <div class="col-sm">
+    <a href="software_image/sublime.jpg">
+     <img src="software_image/sublime.jpg"  alt="Sublime" width="300" height="200">
+    </a>
+    <h9 class="font-weight-bold" class="font-italic" style="font-family:Verdana;">Sublime</h9>
+  <a href="zips/sublime.zip" class="btn btn-outline get-repo-btn" data-ga-click="Repository,download zip,location:repo overview">
+  Download zip
+  </a>
+    </div>
+     <div class="col-sm">
+    <a href="software_image/teamviewer.jpg">
+     <img src="software_image/teamviewer.jpg"  alt="Teamviewer" width="300" height="200">
+    </a>
+    <h9 class="font-weight-bold" class="font-italic" style="font-family:Verdana;">Team Viewer</h9>
+  <a href="zips/teamviewer.zip" class="btn btn-outline get-repo-btn" data-ga-click="Repository,download zip,location:repo overview">
+  Download zip
+  </a>
+    </div>
+     <div class="col-sm">
+    <a href="software_image/vlc.jpg">
+     <img src="software_image/vlc.jpg"  alt="Geary" width="300" height="200">
+    </a>
+    <h9 class="font-weight-bold" class="font-italic" style="font-family:Verdana;">V.L.C</h9>
+  <a href="zips/vlc.zip" class="btn btn-outline get-repo-btn" data-ga-click="Repository,download zip,location:repo overview">
+  Download zip
+  </a>
+    </div>
+    
+
+  </div>
+</div>
+
+    
+    </section>
+    </div>
+
+    </body>
+    </html>
